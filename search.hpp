add3396831cc5552662c89d793f5508eb4c828e0
primenumber <primenumber.proj@gmail.com>
2015-03-19 00:59:11 +0900

    add negaalpha

--- search.hpp
+++ search.hpp
@@ -1,14 +1,44 @@
 #pragma once
+#include <algorithm>
 #include "board.hpp"
 
 template <typename ValueFunc>
-int negamax(const board & bd, int depth, ValueFunc func) {
-  if (depth) {
+int negamax(const board & bd, const int depth, ValueFunc func) {
+  if (depth == 0) {
     return func(bd);
   } else {
     int maxv = -10000;
     for (auto nx : next_states(bd))
-      maxv = max(maxv, -negamax(nx, depth - 1, func));
+      maxv = std::max(maxv, -negamax(nx, depth - 1, func));
     return maxv;
   }
+}
+
+template <typename ValueFunc>
+int negaalpha(const board & bd, const int depth, ValueFunc func,
+    int alpha, int beta) {
+  if (depth == 0) {
+    return func(bd);
+  } else {
+    for (auto nx : next_states(bd)) {
+      int val = -negaalpha(nx, depth - 1, func, -beta, -alpha);
+      if (beta <= val) return beta;
+      alpha = std::max(alpha, val);
+    }
+    return alpha;
+  }
+} 
+
+template <typename ValueFunc>
+board get_opt(const board & bd, const int depth, ValueFunc func) {
+  board res;
+  int maxv = -10000;
+  for (auto nx : next_states(bd)) {
+    int val = -negaalpha(nx, depth - 1, func, -10000, 10000);
+    if (val > maxv) {
+      maxv = val;
+      res = nx;
+    }
+  }
+  return res;
 }
