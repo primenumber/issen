a150fa5c5a136f5db1b56e00a2d98b6ee6c021c5
prime <prime@kmc.gr.jp>
2015-03-20 03:48:52 +0900

    fix nagaalpha

--- search.hpp
+++ search.hpp
@@ -19,8 +19,10 @@ int negamax(const board & bd, const int depth, ValueFunc func) {
 template <typename ValueFunc>
 int negaalpha(const board & bd, const int depth, ValueFunc func,
     int alpha, int beta) {
-  if (depth == 0) {
+  if (depth == 0 || state::is_gameover(bd)) {
     return func(bd);
+  } else if (state::puttable_black(bd) == 0) {
+    return -negaalpha(board(bd, reverse_construct_t()), depth - 1, func, -beta, -alpha);
   } else {
     for (auto nx : state::next_states(bd)) {
       int val = -negaalpha(nx, depth - 1, func, -beta, -alpha);
