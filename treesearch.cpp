16439b2c5e3e2dbed2c3dab9558634182b8ccccf
prime <prime@kmc.gr.jp>
2015-03-21 17:25:03 +0900

    fix expand

--- treesearch.cpp
+++ treesearch.cpp
@@ -1,11 +1,19 @@
+#include <algorithm>
+
 #include "treesearch.hpp"
+
+namespace treesearch {
 
 void expand(node &nd, const board &bd) {
   for (auto nx : state::next_states(bd)) {
     auto child = std::make_unique<node>(nx);
     child->value.push_back(value::value(nx));
-    nd.chilidren.push_back(child);
+    nd.children.push_back(std::move(child));
   }
   std::sort(begin(nd.children), end(nd.children),
-      [](auto lhs, auto rhs) { return lhs->value.front() < rhs->value.front(); });
+      [](std::unique_ptr<node> &lhs, std::unique_ptr<node> &rhs) {
+        return lhs->value.front() < rhs->value.front();
+      });
 }
+
+} // namespace treesearch
