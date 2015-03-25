65bdcec74207e18cbe955b5980f5c4e66cfa73bb
prime <prime@kmc.gr.jp>
2015-03-26 01:38:05 +0900

    divide value

--- value.cpp
+++ value.cpp
@@ -10,9 +10,8 @@ int puttable_value(const board &bd) {
       _popcnt64(state::puttable_black(board::reverse_board(bd))));
 }
 
-int value(const board & bd) {
-  if (state::puttable_black(bd) == 0) return num_value(bd);
-  std::array<int, 64> ary = {{
+int pos_value(const board &bd) {
+  const std::array<int, 64> ary = {{
     100, -50,  15,  15,  15,  15, -50, 100,
     -50, -75, -10, -10, -10, -10, -75, -50,
      15, -10,   0,  -1,  -1,   0, -10,  15,
@@ -22,11 +21,19 @@ int value(const board & bd) {
     -50, -75, -10, -10, -10, -10, -75, -50,
     100, -50,  15,  15,  15,  15, -50, 100
   }};
-  int score = puttable_value(bd);
+  int score = 0;
   for (int i = 0; i < 64; ++i) {
     if (bd.black.get(i)) score += ary[i];
     else if (bd.white.get(i)) score -= ary[i];
   }
+  return score;
+}
+
+int value(const board & bd) {
+  if (state::puttable_black(bd) == 0) return num_value(bd);
+  int score = 0;
+  score += puttable_value(bd);
+  score += pos_value(bd);
   return score;
 }
 
