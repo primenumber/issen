79eb09c8427aadfaa1f593fe15cdfa6f9430f6e0
prime <prime@kmc.gr.jp>
2015-03-25 20:43:28 +0900

    impl puttable_value

--- value.cpp
+++ value.cpp
@@ -5,19 +5,24 @@ namespace value {
 void init() {
 }
 
+int puttable_value(const board &bd) {
+  return 10 * (_popcnt64(state::puttable_black(bd)) -
+      _popcnt64(state::puttable_black(board::reverse_board(bd))));
+}
+
 int value(const board & bd) {
   if (state::puttable_black(bd) == 0) return num_value(bd);
   std::array<int, 64> ary = {{
     100, -50,  15,  15,  15,  15, -50, 100,
     -50, -75, -10, -10, -10, -10, -75, -50,
-     15, -10,   0,  -5,  -5,   0, -10,  15,
-     15, -10,  -5,  -5,  -5,  -5, -10,  15,
-     15, -10,  -5,  -5,  -5,  -5, -10,  15,
-     15, -10,   0,  -5,  -5,   0, -10,  15,
+     15, -10,   0,  -1,  -1,   0, -10,  15,
+     15, -10,  -1,  -1,  -1,  -1, -10,  15,
+     15, -10,  -1,  -1,  -1,  -1, -10,  15,
+     15, -10,   0,  -1,  -1,   0, -10,  15,
     -50, -75, -10, -10, -10, -10, -75, -50,
     100, -50,  15,  15,  15,  15, -50, 100
   }};
-  int score = 0;
+  int score = puttable_value(bd);
   for (int i = 0; i < 64; ++i) {
     if (bd.black.get(i)) score += ary[i];
     else if (bd.white.get(i)) score -= ary[i];
