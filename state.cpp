6640fc9141b69485f4f2d1a7e30e9010b6bd88d3
prime <prime@kmc.gr.jp>
2015-03-26 18:30:46 +0900

    optimize put_black_at

--- state.cpp
+++ state.cpp
@@ -1,5 +1,10 @@
 #include "state.hpp"
 
+#include <cassert>
+
+#include <iomanip>
+#include <iostream>
+
 #include "bit_manipulations.hpp"
 #include "line.hpp"
 
@@ -140,7 +145,7 @@ void put_black_at_dir(board &bd, int i, int j, int dir) {
   }
 }
 
-board put_black_at(const board & bd, int i, int j) {
+board put_black_at_naive(const board & bd, int i, int j) {
   board res = bd;
   for (int dir = 0; dir < 8; ++dir)
     put_black_at_dir(res, i, j, dir);
@@ -148,18 +153,58 @@ board put_black_at(const board & bd, int i, int j) {
   return res;
 }
 
+uint64_t put_black_at_horizontal(const board &bd, int i, int j) {
+  return ((uint64_t)line::put_line(bd, i, j, 0)) << (i * 8);
+}
+
+uint64_t put_black_at_vertical(const board &bd, int i, int j) {
+  return bit_manipulations::flipDiagA1H8(
+      ((uint64_t)line::put_line(
+          bit_manipulations::flipDiagA1H8(bd), j, i, 0)) << (j * 8));
+}
+
+uint64_t put_black_at_diag_implA8H1(const board &bd, int i, int j) {
+  board prot45_bd = 
+      bit_manipulations::pseudoRotate45clockwise(bd);
+  return bit_manipulations::pseudoRotate45antiClockwise(
+      ((uint64_t)line::put_line(prot45_bd,
+          (i + j + 1) % 8, j, (i + j + 1) % 8)) << (((i + j + 1) % 8) * 8));
+}
+
+uint64_t put_black_at_diag_implA1H8(const board &bd, int i, int j) {
+  board prot45a_bd = 
+      bit_manipulations::pseudoRotate45antiClockwise(bd);
+  return bit_manipulations::pseudoRotate45clockwise(
+      ((uint64_t)line::put_line(prot45a_bd,
+          (i + 8 - j) % 8, j, (8 - i + j) % 8)) << (((i + 8 - j) % 8) * 8));
+}
+
+uint64_t put_black_at_diag(const board &bd, int i, int j) {
+  return bit_manipulations::rotr(
+      put_black_at_diag_implA1H8(bd, i, j) |
+      put_black_at_diag_implA8H1(bd, i, j), 8);
+}
+
+board put_black_at(const board & bd, int i, int j) {
+  uint64_t reverse_bits = put_black_at_horizontal(bd, i, j) |
+      put_black_at_vertical(bd, i, j) |
+      put_black_at_diag(bd, i, j);
+  return board(
+      bd.black.data ^ reverse_bits |
+          UINT64_C(1) << (i * 8 + j),
+      bd.white.data ^ reverse_bits);
+}
+
 std::vector<board> next_states(const board & bd) {
   std::vector<board> res;
   res.reserve(16);
   bool is_pass = true;
-  uint64_t puttables = puttable_black(bd);
-  for (int i = 0; i < 8; ++i)
-    for (int j = 0; j < 8; ++j)
-      if ((puttables >> (i*8+j)) & 1) {
-        res.emplace_back(put_black_at(bd, i, j),
-            reverse_construct_t());
-        is_pass = false;
-      }
+  for (uint64_t bits = puttable_black(bd); bits != 0; bits &= bits - 1) {
+    int pos = bit_manipulations::bit_to_pos(bits & -bits);
+    res.emplace_back(put_black_at(bd, pos / 8, pos % 8),
+        reverse_construct_t());
+    is_pass = false;
+  }
   if (is_pass) res.emplace_back(bd, reverse_construct_t());
   return res;
 }
