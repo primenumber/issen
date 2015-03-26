06e567d928cc2b1a812f458f2eb928fd3a751fc7
prime <prime@kmc.gr.jp>
2015-03-26 18:30:02 +0900

    memorize put_line

--- line.cpp
+++ line.cpp
@@ -10,8 +10,11 @@
 namespace line {
 
 std::array<std::array<uint8_t, 6561>, 8> puttable_line_table;
+std::array<std::array<std::array<uint8_t, 8>, 6561>, 8>
+    put_line_table;
 
 uint8_t puttable_line_naive(const board &, int, int);
+uint8_t put_line_naive(const board &, int, int, int);
 
 void init() {
   for (int i = 0; i < 256; ++i) {
@@ -21,6 +24,12 @@ void init() {
       int index = bit_manipulations::toBase3(i, j);
       for (int k = 0; k < 8; ++k) {
         puttable_line_table[k][index] = puttable_line_naive(bd, 0, k);
+        for (int l = 0; l < 8; ++l) {
+          if ((puttable_line_table[k][index] >> l) & 1) {
+            put_line_table[k][index][l] = put_line_naive(bd, 0, l, k);
+            assert(put_line_table[k][index][l] != 0);
+          }
+        }
       }
     }
   }
@@ -50,7 +59,7 @@ bool is_puttable_line_backward(const board &bd, int line8,
   return false;
 }
 
-bool is_puttable_line(const board &bd, int line,
+bool is_puttable_line_naive(const board &bd, int line,
     int pos, int separator) {
   line *= 8;
   if (bd.black.get(line + pos) || bd.white.get(line + pos))
@@ -62,15 +71,77 @@ bool is_puttable_line(const board &bd, int line,
 uint8_t puttable_line_naive(const board &bd, int line, int separator) {
   uint8_t res = 0;
   for (int i = 0; i < 8; ++i)
-    if (is_puttable_line(bd, line, i, separator))
+    if (is_puttable_line_naive(bd, line, i, separator))
       res |= 1 << i;
   return res;
+}
+
+bool is_puttable_line(const board &bd, int line,
+    int pos, int separator) {
+  return (puttable_line(bd, line, separator) >> pos) & 1;
+}
+
+bool is_puttable_line(uint8_t black, uint8_t white,
+    int pos, int separator) {
+  return (puttable_line(black, white, separator) >> pos) & 1;
 }
 
 uint8_t puttable_line(const board &bd, int line, int separator) {
   uint8_t bb = bd.black.data >> (line * 8);
   uint8_t wb = bd.white.data >> (line * 8);
   return puttable_line_table[separator][bit_manipulations::toBase3(bb, wb)];
+}
+
+uint8_t puttable_line(uint8_t black, uint8_t white, int separator) {
+  return puttable_line_table[separator]
+    [bit_manipulations::toBase3(black, white)];
+}
+
+uint8_t put_line_forward(const board &bd, int line8, int pos, int separator) {
+  int limit = (pos >= separator) ? 8 : separator;
+  uint8_t res = 0;
+  for (int i = pos + 1; i < limit; ++i) {
+    if (bd.white.get(line8 + i)) {
+      res |= 1 << i;
+    } else if (bd.black.get(line8 + i)) {
+      return res;
+    } else {
+      return 0;
+    }
+  }
+  return 0;
+}
+
+uint8_t put_line_backward(const board &bd, int line8, int pos, int separator) {
+  int limit = (pos >= separator) ? separator : 0;
+  uint8_t res = 0;
+  for (int i = pos - 1; i >= limit; --i) {
+    if (bd.white.get(line8 + i)) {
+      res |= 1 << i;
+    } else if (bd.black.get(line8 + i)) {
+      return res;
+    } else {
+      return 0;
+    }
+  }
+  return 0;
+}
+
+uint8_t put_line_naive(const board &bd, int line, int pos, int separator) {
+  return put_line_forward(bd, line, pos, separator) |
+      put_line_backward(bd, line, pos, separator);
+}
+
+uint8_t put_line(const board &bd, int line, int pos, int separator) {
+  uint8_t bb = bd.black.data >> (line * 8);
+  uint8_t wb = bd.white.data >> (line * 8);
+  return put_line_table[separator]
+      [bit_manipulations::toBase3(bb, wb)][pos];
+}
+
+uint8_t put_line(uint8_t black, uint8_t white, int pos, int separator) {
+  return put_line_table[separator]
+      [bit_manipulations::toBase3(black, white)][pos];
 }
 
 } // namespace line;
--- line.hpp
+++ line.hpp
@@ -6,7 +6,11 @@ namespace line {
 
 void init();
 
-bool is_puttable_line(const board &, int, int, int);
-uint8_t puttable_line(const board &, int, int);
+bool is_puttable_line(const board &, int line, int pos, int separator);
+bool is_puttable_line(uint8_t black, uint8_t white, int pos, int separator);
+uint8_t puttable_line(const board &, int line, int separator);
+uint8_t puttable_line(uint8_t black, uint8_t white, int separator);
+uint8_t put_line(const board &, int line, int pos, int separator);
+uint8_t put_line(uint8_t black, uint8_t white, int pos, int separator);
 
 } // namespace line
