6d25ea52edd4ac2fca0c823967c40516fd24b0e6
prime <prime@kmc.gr.jp>
2015-04-04 21:19:38 +0900

    refactor value functions

--- value.cpp
+++ value.cpp
@@ -1,31 +1,62 @@
 #include "value.hpp"
+
+#include <iostream>
+
 #include "bit_manipulations.hpp"
+#include "utils.hpp"
 
 namespace value {
 
 void init() {
 }
 
+int diff_num(const board &bd) {
+  int bn = _popcnt64(bd.black.data);
+  int wn = _popcnt64(bd.white.data);
+  return bn - wn;
+}
+
+int fixed_diff_num(const board &bd) {
+  int bn = _popcnt64(bd.black.data);
+  int wn = _popcnt64(bd.white.data);
+  if (bn) {
+    if (wn) return bn - wn;
+    else return 64;
+  } else {
+    return -64;
+  }
+}
+
 int puttable_value(const board &bd) {
-  return 10 * (_popcnt64(state::puttable_black(bd)) -
-      _popcnt64(state::puttable_black(board::reverse_board(bd))));
+  int b = _popcnt64(state::puttable_black(bd));
+  int w = _popcnt64(state::puttable_black(board::reverse_board(bd)));
+  if (b) {
+    if (w) {
+      return 10 * (b - w);
+    } else {
+      return 10 * 64;
+    }
+  } else {
+    return -10 * 64;
+  }
 }
 
 int definite_value(const board &bd) {
-  return num_value(bit_manipulations::definites(bd));
+  board df = bit_manipulations::definites(bd);
+  return diff_num(df) * 50;
 }
 
 int pos_value(const board &bd) {
   int score = 0;
   const std::array<int, 64> ary = {{
-    100, -50,  15,  15,  15,  15, -50, 100,
-    -50, -75, -10, -10, -10, -10, -75, -50,
-     15, -10,   0,  -3,  -3,   0, -10,  15,
-     15, -10,  -3,  -3,  -3,  -3, -10,  15,
-     15, -10,  -3,  -3,  -3,  -3, -10,  15,
-     15, -10,   0,  -3,  -3,   0, -10,  15,
-    -50, -75, -10, -10, -10, -10, -75, -50,
-    100, -50,  15,  15,  15,  15, -50, 100
+    100, -40,   5,   0,   0,   5, -40, 100,
+    -40, -50, -10, -10, -10, -10, -50, -40,
+      5, -10,   0,  -3,  -3,   0, -10,   5,
+      0, -10,  -3,  -3,  -3,  -3, -10,   0,
+      0, -10,  -3,  -3,  -3,  -3, -10,   0,
+      5, -10,   0,  -3,  -3,   0, -10,   5,
+    -40, -50, -10, -10, -10, -10, -50, -40,
+    100, -40,   5,   0,   0,   5, -40, 100
   }};
   for (int i = 0; i < 64; ++i) {
     if (bd.black.get(i)) score += ary[i];
@@ -35,16 +66,16 @@ int pos_value(const board &bd) {
 }
 
 int value(const board & bd) {
-  if (state::puttable_black(bd) == 0) return num_value(bd);
+  //if (state::puttable_black(bd) == 0) return num_value(bd);
   int score = 0;
   score += puttable_value(bd);
-  score += pos_value(bd);
-  //score += definite_value(bd)/2;
+  //score += pos_value(bd);
+  score += definite_value(bd)/4;
   return score;
 }
 
 int num_value(const board & bd) {
-  return 100 * (_popcnt64(bd.black.data) - _popcnt64(bd.white.data));
+  return 100 * fixed_diff_num(bd);
 }
 
 } // namespace value
--- value.hpp
+++ value.hpp
@@ -8,6 +8,8 @@ constexpr int VALUE_MAX = 100000;
 
 void init();
 int value(const board &);
+int diff_num(const board &);
+int fixed_diff_num(const board &);
 int num_value(const board &);
 
 } // namespace value
