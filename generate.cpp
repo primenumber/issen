8b7670aa9a2a291983a11a32114b66ab6c3170d1
prime <prime@kmc.gr.jp>
2015-06-07 13:54:33 +0900

    endsearch for generate

--- generate.cpp
+++ generate.cpp
@@ -4,10 +4,12 @@
 
 #include <random>
 
+#include "bit_manipulations.hpp"
 #include "state.hpp"
 #include "hand.hpp"
 #include "utils.hpp"
 #include "value.hpp"
+#include "tree_manager.hpp"
 
 namespace generator {
 
@@ -22,11 +24,28 @@ std::vector<board> generate(const board &bd) {
   std::vector<board> ary;
   ary.push_back(bd);
   board tmp = bd;
+  bool is_black = true;
   while (!state::is_gameover(tmp)) {
+    if (bit_manipulations::stone_sum(tmp) == 54) break;
     auto nexts = state::next_states(tmp);
     std::uniform_int_distribution<> dis(0, nexts.size() - 1);
     tmp = nexts[dis(mt)];
-    ary.push_back(tmp);
+    is_black = !is_black;
+    if (is_black)
+      ary.push_back(tmp);
+    else
+      ary.emplace_back(tmp, reverse_construct_t());
+  }
+  if (!state::is_gameover(tmp)) {
+    tree_manager::tree_manager tm(tmp, is_black);
+    auto tp = tm.endgame_search();
+    for (auto p : std::get<0>(tp)) {
+      is_black = !is_black;
+      if (is_black)
+        ary.push_back(std::get<0>(p));
+      else
+        ary.emplace_back(std::get<0>(p), reverse_construct_t());
+    }
   }
   return ary;
 }
@@ -36,6 +55,7 @@ void generate_record(const board &bd, size_t n) {
   std::printf("Black\n");
   std::printf("%zd\n", n);
   for (int i = 0; i < n; ++i) {
+    if ((i % 1000) == 0) fprintf(stderr, "%d\n", i);
     auto board_record = generate(bd);
     for (int j = 0; j < board_record.size() - 1; ++j) {
       hand h = hand_from_diff(board_record[j], board_record[j+1]);
