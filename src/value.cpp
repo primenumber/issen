f7d2a87dc9455b79bad88cfa474503df385ba78d
prime <prime@kmc.gr.jp>
2015-07-29 22:54:18 +0900

    fix bugs ggs_archive_parser

--- src/value.cpp
+++ src/value.cpp
@@ -13,28 +13,15 @@
 
 namespace value {
 
-std::string files[4] = {"lsa1m12", "lsa1m14", "lsa1m16", "lsa1m18"};
-std::vector<std::vector<double>> vals;
-int val_indeces[60] = {
-  0,0,0,0,0,0,0,0,0,0,
-  0,0,0,1,1,2,2,3,3,3,
-  3,3,3,3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,3,3,3};
-
-void load16() {
-}
+std::string value_file_name = "lsa300k_12";
+std::vector<double> vals;
 
 void init() {
-  vals.resize(4);
-  for (int cnt = 0; cnt < 4; ++cnt) {
-    std::ifstream ifs(files[cnt]);
-    for (int i = 0; i <= subboard::index_max; ++i) {
-      double v;
-      ifs >> v;
-      vals[cnt].push_back(v);
-    }
+  std::ifstream ifs(value_file_name);
+  for (int i = 0; i <= subboard::index_max; ++i) {
+    double v;
+    ifs >> v;
+    vals.push_back(v);
   }
 }
 
@@ -132,12 +119,11 @@ int num_value(const board & bd) {
 }
 
 int statistic_value (const board &bd) {
-  int index = val_indeces[64 - bit_manipulations::stone_sum(bd)];
   std::vector<int> indeces = subboard::serialize(bd);
   double res = 0;
   assert(indeces.size() == 46);
   for (int i : indeces) {
-    res += vals[index][i];
+    res += vals[i];
   }
   return res * 100;
 }
