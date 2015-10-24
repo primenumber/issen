91961ce019f444e5b2ca58438cebd1fdabeab737
prime <prime@kmc.gr.jp>
2015-10-25 06:00:35 +0900

    fix default constructor of game_record

--- include/record.hpp
+++ include/record.hpp
@@ -14,7 +14,7 @@ struct game_record {
       : hands(std::move(h)), result(res) {}
   game_record(const game_record &gr) = default;
   game_record(game_record &&gr) = default;
-  game_record() = default;
+  game_record() : hands(), result() {};
   game_record &operator=(const game_record &gr) = default;
   game_record &operator=(game_record &&gr) = default;
 };
