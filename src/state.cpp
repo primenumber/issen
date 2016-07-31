4cb3f7ced2d2b0bc5fe00ce70fb8d848fe396d9c
primenumber <prime@kmc.gr.jp>
2016-08-01 01:13:33 +0900

    implement flip

--- src/state.cpp
+++ src/state.cpp
@@ -8,6 +8,8 @@
 #include "bit_manipulations.hpp"
 #include "line.hpp"
 
+#include "immintrin.h"
+
 namespace bm = bit_manipulations;
 
 namespace state {
@@ -135,42 +137,114 @@ board put_black_at_naive(const board & bd, int i, int j) {
   return res;
 }
 
-uint64_t put_black_at_horizontal(const board &bd, int i, int j) {
-  return ((uint64_t)line::put_line(bd, i, j, 0)) << (i * 8);
+struct u64_4 {
+  __m128i xy, zw;
+  u64_4() = default;
+  u64_4(uint64_t val)
+    : xy(_mm_set1_epi64x(val)), zw(_mm_set1_epi64x(val)) {}
+  u64_4(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
+    : xy(_mm_set_epi64x(x, y)), zw(_mm_set_epi64x(z, w)) {}
+  u64_4(__m128i xy, __m128i zw)
+    : xy(xy), zw(zw) {}
+};
+
+u64_4 operator>>(const u64_4 lhs, const size_t n) {
+  return u64_4(_mm_srli_epi64(lhs.xy, n), _mm_srli_epi64(lhs.zw, n));
 }
 
-uint64_t put_black_at_vertical(const board &bd, int i, int j) {
-  return bm::flipDiagA1H8(
-      ((uint64_t)line::put_line(
-          bm::flipDiagA1H8(bd), j, i, 0)) << (j * 8));
+u64_4 operator<<(const u64_4 lhs, const size_t n) {
+  return u64_4(_mm_slli_epi64(lhs.xy, n), _mm_slli_epi64(lhs.zw, n));
 }
 
-uint64_t put_black_at_diag_implA8H1(const board &bd, int i, int j) {
-  board prot45_bd = 
-      bm::pseudoRotate45clockwise(bd);
-  return bm::pseudoRotate45antiClockwise(
-      ((uint64_t)line::put_line(prot45_bd,
-          (i + j + 1) & 7, j, (i + j + 1) & 7)) << (((i + j + 1) & 7) * 8));
+u64_4 operator&(const u64_4 lhs, const u64_4 rhs) {
+  return u64_4(_mm_and_si128(lhs.xy, rhs.xy), _mm_and_si128(lhs.zw, rhs.zw));
 }
 
-uint64_t put_black_at_diag_implA1H8(const board &bd, int i, int j) {
-  board prot45a_bd = 
-      bm::pseudoRotate45antiClockwise(bd);
-  return bm::pseudoRotate45clockwise(
-      ((uint64_t)line::put_line(prot45a_bd,
-          (i - j) & 7, j, (- i + j) & 7)) << (((i - j) & 7) * 8));
+u64_4 operator&(const u64_4 lhs, const uint64_t rhs) {
+  __m128i r64 = _mm_set1_epi64x(rhs);
+  return u64_4(_mm_and_si128(lhs.xy, r64), _mm_and_si128(lhs.zw, r64));
 }
 
-uint64_t put_black_at_diag(const board &bd, int i, int j) {
-  return bm::rotr(
-      put_black_at_diag_implA1H8(bd, i, j) |
-      put_black_at_diag_implA8H1(bd, i, j), 8);
+u64_4 operator|(const u64_4 lhs, const u64_4 rhs) {
+  return u64_4(_mm_or_si128(lhs.xy, rhs.xy), _mm_or_si128(lhs.zw, rhs.zw));
+}
+
+u64_4 operator|(const u64_4 lhs, const uint64_t rhs) {
+  __m128i r64 = _mm_set1_epi64x(rhs);
+  return u64_4(_mm_or_si128(lhs.xy, r64), _mm_or_si128(lhs.zw, r64));
+}
+
+u64_4 operator+(const u64_4 lhs, const uint64_t rhs) {
+  __m128i r64 = _mm_set1_epi64x(rhs);
+  return u64_4(_mm_add_epi64(lhs.xy, r64), _mm_add_epi64(lhs.zw, r64));
+}
+
+u64_4 operator+(const u64_4 lhs, const u64_4 rhs) {
+  return u64_4(_mm_add_epi64(lhs.xy, rhs.xy), _mm_add_epi64(lhs.zw, rhs.zw));
+}
+
+u64_4 operator-(const u64_4 lhs, const u64_4 rhs) {
+  return u64_4(_mm_sub_epi64(lhs.xy, rhs.xy), _mm_sub_epi64(lhs.zw, rhs.zw));
+}
+
+u64_4 operator!=(const u64_4 lhs, const uint64_t rhs) {
+  __m128i r64 = _mm_set1_epi64x(rhs);
+  return u64_4(_mm_cmpeq_epi64(lhs.xy, r64), _mm_cmpeq_epi64(lhs.zw, r64)) + u64_4(1);
+}
+
+u64_4 operator-(const u64_4 lhs) {
+  return u64_4(_mm_sub_epi64(_mm_setzero_si128(), lhs.xy), _mm_sub_epi64(_mm_setzero_si128(), lhs.zw));
+}
+
+// (NOT lhs) AND rhs
+u64_4 andnot(const u64_4 lhs, const u64_4 rhs) {
+  return u64_4(_mm_andnot_si128(lhs.xy, rhs.xy), _mm_andnot_si128(lhs.zw, rhs.zw));
+}
+
+u64_4 operator~(const u64_4 lhs) {
+  return u64_4(_mm_andnot_si128(lhs.xy, _mm_set1_epi8(0xFF)), _mm_andnot_si128(lhs.zw, _mm_set1_epi8(0xFF)));
+}
+
+u64_4 upper_bit(u64_4 p) {
+  p = p | (p >> 1);
+  p = p | (p >> 2);
+  p = p | (p >> 4);
+  p = p | (p >> 8);
+  p = p | (p >> 16);
+  p = p | (p >> 32);
+  return andnot(p >> 1, p);
+}
+
+uint64_t flip(const board &bd, int pos) {
+  u64_4 flipped, OM, outflank, mask;
+  uint64_t yzw = bd.white() & UINT64_C(0x7E7E7E7E7E7E7E7E);
+  OM = u64_4(bd.white(), yzw, yzw, yzw);
+  mask = {
+    UINT64_C(0x0080808080808080),
+    UINT64_C(0x7F00000000000000),
+    UINT64_C(0x0102040810204000),
+    UINT64_C(0x0040201008040201)
+  };
+  mask = mask >> (63 - pos);
+  outflank = upper_bit(andnot(OM, mask)) & bd.black();
+  flipped = (-outflank << 1) & mask;
+  mask = {
+    UINT64_C(0x0101010101010100),
+    UINT64_C(0x00000000000000FE),
+    UINT64_C(0x0002040810204080),
+    UINT64_C(0x8040201008040200)
+  };
+  mask = mask << pos;
+  outflank = mask & ((OM | ~mask) + 1) & bd.black();
+  flipped = flipped | ((outflank - (outflank != 0)) & mask);
+  __m128i flipped_xz_yw = _mm_or_si128(flipped.xy, flipped.zw);
+  uint64_t lo = _mm_cvtsi128_si64(flipped_xz_yw);
+  uint64_t hi = _mm_cvtsi128_si64(_mm_srli_si128(flipped_xz_yw, 8));
+  return lo | hi;
 }
 
 board put_black_at(const board & bd, int i, int j) {
-  uint64_t reverse_bits = put_black_at_horizontal(bd, i, j) |
-      put_black_at_vertical(bd, i, j) |
-      put_black_at_diag(bd, i, j);
+  uint64_t reverse_bits = flip(bd, i*8 + j);
   return board(
       (bd.black() ^ reverse_bits) |
           UINT64_C(1) << (i * 8 + j),
@@ -178,9 +252,7 @@ board put_black_at(const board & bd, int i, int j) {
 }
 
 board put_black_at_rev(const board & bd, int i, int j) {
-  uint64_t reverse_bits = put_black_at_horizontal(bd, i, j) |
-      put_black_at_vertical(bd, i, j) |
-      put_black_at_diag(bd, i, j);
+  uint64_t reverse_bits = flip(bd, i*8 + j);
   return board(
       bd.white() ^ reverse_bits,
       (bd.black() ^ reverse_bits) |
