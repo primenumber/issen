381b7be07a8929ac9ceb83c1fd7b87c8879a9aab
prime <prime@kmc.gr.jp>
2016-09-08 22:30:38 +0900

    fix _mm256_set*

--- include/gcc_intrinsics.hpp
+++ include/gcc_intrinsics.hpp
@@ -23,9 +23,9 @@ inline __m128i _mm_setr_epi64x(uint64_t lo, uint64_t hi) {
 }
 
 inline __m256i _mm256_set_m128i(__m128i hi, __m128i lo) {
-  return _mm256_inserti128_si256(_mm256_castsi128_si256(hi), lo, 1);
+  return _mm256_inserti128_si256(_mm256_castsi128_si256(lo), hi, 1);
 }
 inline __m256i _mm256_setr_m128i(__m128i lo, __m128i hi) {
-  return _mm256_inserti128_si256(_mm256_castsi128_si256(hi), lo, 1);
+  return _mm256_inserti128_si256(_mm256_castsi128_si256(lo), hi, 1);
 }
 #endif
