03133e0b869bcaa6beb33975542b2b1af64f5bf7
prime <prime@kmc.gr.jp>
2019-01-03 00:25:11 +0900

    remove intrinsics

--- include/gcc_intrinsics.hpp
+++ include/gcc_intrinsics.hpp
@@ -21,11 +21,4 @@ constexpr bool _bittestandreset64(uint64_t * const bits, int index) {
 inline __m128i _mm_setr_epi64x(uint64_t lo, uint64_t hi) {
   return _mm_set_epi64x(hi, lo);
 }
-
-inline __m256i _mm256_set_m128i(__m128i hi, __m128i lo) {
-  return _mm256_inserti128_si256(_mm256_castsi128_si256(lo), hi, 1);
-}
-inline __m256i _mm256_setr_m128i(__m128i lo, __m128i hi) {
-  return _mm256_inserti128_si256(_mm256_castsi128_si256(lo), hi, 1);
-}
 #endif
